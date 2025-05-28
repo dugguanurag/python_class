b521ed26545437044f9db566017916d46d6f672c
Manoj-Sh-AI <shmanoj100@gmail.com>
2025-05-28 15:50:10 +0200

    Branch 4 added

--- python_names.py
+++ python_names.py
@@ -1,1 +1,8 @@
 # test
+def print_full_name(first, family):
+    return first + " " + family
+
+first = "Manoj"
+family = "Saligrama Harisha - doing again & again"
+result = print_full_name(first, family)
+print(result)
